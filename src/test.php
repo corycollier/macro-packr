6b7bf692f40f006466cb4ae374f156d5b4150c53
Cory Collier <cory.collier@ahss.org>
2017-02-07 15:38:50 -0500

    Nothing concrete yet. just committing my thoughts here

--- src/test.php
+++ src/test.php
@@ -19,6 +19,24 @@ $items = [
         'carb' => 25,
         'protein' => 8,
     ],
+    [
+        'name' => 'random 1',
+        'fat' => 0,
+        'carb' => 25,
+        'protein' => 0,
+    ],
+    [
+        'name' => 'random 2',
+        'fat' => 55,
+        'carb' => 5,
+        'protein' => 6,
+    ],
+    [
+        'name' => 'random 3',
+        'fat' => 15,
+        'carb' => 55,
+        'protein' => 18,
+    ],
 ];
 
 $max = [
@@ -32,3 +50,69 @@ $state = [
     'protein' => '90',
     'fat' => '68',
 ];
+
+
+// determine numbers remaining
+function get_leftovers($max, $state) {
+    $results = [];
+
+    foreach ($state as $type => $value) {
+        $results[$type] = $max[$type] - $state[$type];
+    }
+    return $results;
+}
+
+function sort_items($items) {
+    $results = [];
+    foreach ($items as $item) {
+        $calories = get_calories($item);
+        $results[$calories] = $item;
+    }
+
+    krsort($results);
+
+    return $results;
+}
+
+function get_calories($item) {
+    $defaults = [
+        'fat' => 0,
+        'protein' => 0,
+        'carbs' => 0,
+    ];
+
+    $item = array_merge($defaults, $item);
+
+    return ($item['fat'] * 9
+        + $item['carbs'] * 4
+        + $item['protein'] * 4
+    );
+}
+
+function filter_by_leftovers($leftovers, $items) {
+    foreach ($items as $index => $item) {
+        if (is_over_numbers($leftovers, $item)) {
+            unset($items[$index]);
+            echo 'found one', $item['name'], PHP_EOL;
+        }
+    }
+    return $items;
+}
+
+function is_over_numbers($leftovers, $item) {
+    foreach ($leftovers as $type => $value) {
+        if ($item[$type] > $value) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+$leftovers = get_leftovers($max, $state);
+
+$sorted = sort_items($items);
+
+$filtered = filter_by_leftovers($leftovers, $sorted);
+
+print_r($filtered);
