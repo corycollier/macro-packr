821b689078caa515eb1f559dc69445d804ee92e6
Cory Collier <cory.collier@ahss.org>
2017-02-07 15:12:36 -0500

    First commit of real project setup

---
+++ src/test.php
@@ -0,0 +1,34 @@
+<?php
+
+$items = [
+    [
+        'name' => 'fat food',
+        'fat' => 20,
+        'carb' => 5,
+        'protein' => 4,
+    ],
+    [
+        'name' => 'protein food',
+        'fat' => 2,
+        'carb' => 5,
+        'protein' => 14,
+    ],
+    [
+        'name' => 'carb food',
+        'fat' => 5,
+        'carb' => 25,
+        'protein' => 8,
+    ],
+];
+
+$max = [
+    'carb'    => '500',
+    'protein' => '300',
+    'fat'     => '89',
+];
+
+$state = [
+    'carb' => '134',
+    'protein' => '90',
+    'fat' => '68',
+];
